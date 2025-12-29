c25964bfff9191a49dc92e09d00bf2fdcb673792
Megood1210 <xuanvinhle06@gmail.com>
2025-12-29 21:15:56 +0700

    Upload bài tập 4

---
+++ Bài4.sql
@@ -0,0 +1,24 @@
+create database session2;
+Use session2;
+
+create table Subject(
+	subject_id int auto_increment primary key,
+    subject_name varchar(20) unique not null,
+    credit_hours int not null CHECK (credit_hours > 0)
+);
+create table Student(
+	student_id int auto_increment primary key,
+    student_name varchar(30) unique not null
+);
+create table Enrollment(
+	student_id int not null,
+    subject_id int not null,
+    enroll_day date not null,
+    primary key(student_id, subject_id),
+    foreign key(student_id) references
+    Student(student_id),
+    foreign key(subject_id) references
+    Subject(subject_id)
+);
+
+
